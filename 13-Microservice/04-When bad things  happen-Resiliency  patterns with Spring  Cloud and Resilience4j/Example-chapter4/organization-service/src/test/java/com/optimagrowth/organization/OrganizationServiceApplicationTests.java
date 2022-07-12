36897a67eef7220631a0a1f855337689f6018635
Hieu <Thanglong@gmail.com>
2022-07-13 00:18:55 +0700

    chapter4

---
+++ 13-Microservice/04-When bad things  happen-Resiliency  patterns with Spring  Cloud and Resilience4j/Example-chapter4/organization-service/src/test/java/com/optimagrowth/organization/OrganizationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optimagrowth.organization;
+
+import org.junit.jupiter.api.Disabled;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizationServiceApplicationTests {
+
+	@Disabled
+	void contextLoads() {
+	}
+
+}
